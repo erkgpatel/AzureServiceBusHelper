dc8b9f5eda0c2cc8e6db20ae6cfb902fccb8f5a6
Keyurkumar Patel <erkgpatel@gmail.com>
2024-07-14 23:33:56 +0530

    class added

---
+++ src/Erkgpatel.AzureServiceBus.Helper/ServiceBusMessageReceiver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erkgpatel.AzureServiceBus.Helper
+{
+    internal class ServiceBusMessageReceiver
+    {
+    }
+}
