951e2ca2dcca4fc457b3dff195d0f7e322a258d9
Keyurkumar Patel <erkgpatel@gmail.com>
2024-08-03 22:01:30 +0530

    update code

--- src/Erkgpatel.AzureServiceBus.Helper/ServiceBusMessageReceiver.cs
+++ src/Erkgpatel.AzureServiceBus.Helper/ServiceBusMessageReceiver.cs
@@ -11,7 +11,6 @@ namespace Erkgpatel.AzureServiceBus.Helper
         public ServiceBusMessageReceiver(IOptions<ServiceBusConfigs> configs)
         {
 
-
             foreach (var config in configs.Value.Configurations)
             {
                 var client = new ServiceBusClient(config.ConnectionString);
